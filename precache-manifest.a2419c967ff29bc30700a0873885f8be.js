c0feed9ce46077043aad8130107be30d8c590703
PathakPratik <rajpratik10@gmail.com>
2019-05-13 00:31:21 +0530

    Updates

---
+++ precache-manifest.a2419c967ff29bc30700a0873885f8be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c39c440bab6c86bae05524996966804",
+    "url": "/DynamicCarousel/index.html"
+  },
+  {
+    "revision": "0a9a047c67f1260efd20",
+    "url": "/DynamicCarousel/static/css/main.c3c453ce.chunk.css"
+  },
+  {
+    "revision": "cbec03855529b35d7fae",
+    "url": "/DynamicCarousel/static/js/2.efdcfd8c.chunk.js"
+  },
+  {
+    "revision": "0a9a047c67f1260efd20",
+    "url": "/DynamicCarousel/static/js/main.163a92e0.chunk.js"
+  },
+  {
+    "revision": "3ec2eff2c7f5aa9aa239",
+    "url": "/DynamicCarousel/static/js/runtime~main.b08df813.js"
+  }
+]);
